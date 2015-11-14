6fbb9df34cb0e0070e4246f955d77e6d3f743e5e
Ghoul Nadir <nadir.ghoul@u-psud.fr>
2015-11-14 03:55:44 +0100

    Fix grill.h
    Create grill.cpp to avoid multiples definition's error

---
+++ grille/grille.cpp
@@ -0,0 +1,54 @@
+#include "grille.h"
+
+Grille grilleVide () {
+    return Grille(GRILL_HEIGHT,Ligne(GRILL_WIDTH,0));
+}
+
+Grille initialiseGrille ( Grille grille ) {
+    return grille;
+}
+
+Grille deplacementGauche( Grille grille ) {
+    return grille;
+}
+
+Grille deplacementDroite( Grille grille ) {
+    return grille;
+}
+
+Grille deplacementHaut( Grille grille ) {
+    return grille;
+}
+
+Grille deplacementBas( Grille grille ) {
+    return grille;
+}
+
+/** Documentation a faire **/
+Grille deplacement( Grille grille , int direction ) {
+    switch ( direction ) {
+        case GAUCHE:
+            return deplacementGauche( grille );
+        case DROITE:
+            return deplacementDroite( grille );
+        case HAUT:
+            return deplacementHaut( grille );
+        case BAS:
+            return deplacementBas( grille );
+        default:
+            cerr << "Deplacement␣non−autorise !" << endl ;
+            exit(-1);
+    }
+}
+
+bool estTerminer( Grille grille ) {
+    return true;
+}
+
+int score( Grille grille ) {
+    return -1;
+}
+
+bool estGagnant( Grille grille ) {
+    return false;
+}
--- grille/grille.h
+++ grille/grille.h
@@ -1,38 +1,12 @@
-#include <vector>
-#include <iostream>
-#include <cstdlib>
+#include "../main.h"
 
-#ifndef MINC
-    #define MINC
-    #include "../main.h"
-#endif // MINC
-
-
-Grille grilleVide () {
-    return Grille(GRILL_HEIGHT,Ligne(GRILL_WIDTH,0));
-}
-Grille initialiseGrille ( Grille grille ) {return grille;}
-Grille deplacementGauche( Grille grille ) {return grille;}
-Grille deplacementDroite( Grille grille ) {return grille;}
-Grille deplacementHaut( Grille grille ) {return grille;}
-Grille deplacementBas( Grille grille ) {return grille;}
-
-/** Documentation a faire **/
-Grille deplacement( Grille grille , int direction ) {
-    switch ( direction ) {
-        case GAUCHE:
-            return deplacementGauche( grille );
-        case DROITE:
-            return deplacementDroite( grille );
-        case HAUT:
-            return deplacementHaut( grille );
-        case BAS:
-            return deplacementBas( grille );
-        default:
-            cerr << "Deplacement␣non−autorise !" << endl ;
-            exit(-1);
-    }
-}
-bool estTerminer( Grille grille ) {return true;}
-int score( Grille grille ) {return -1;}
-bool estGagnant( Grille grille ) {return false;}
+Grille grilleVide ();
+Grille initialiseGrille ( Grille grille );
+Grille deplacementGauche( Grille grille );
+Grille deplacementDroite( Grille grille );
+Grille deplacementHaut( Grille grille );
+Grille deplacementBas( Grille grille );
+Grille deplacement( Grille grille , int direction );
+bool estTerminer( Grille grille );
+int score( Grille grille );
+bool estGagnant( Grille grille );
