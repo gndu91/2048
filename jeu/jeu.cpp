ce3099e5848565725da3dc5fc53fa9929900cb77
Ghoul Nadir <nadir.ghoul@u-psud.fr>
2015-11-21 16:37:10 +0100

    Revert "Revert "Fonction de base et representation du rendu""
    This reverts commit 590b03d49980b0de2ce36f0d72fc4584efb7623f.

--- jeu/jeu.cpp
+++ jeu/jeu.cpp
@@ -2,12 +2,39 @@
  *  @brief  Contain jeu(), in all namespaces
  */
 #include "jeu.h"
+/**
+                                           ╔══════╗
+                                           ║ Game ║
+                                           ╚══════╝
+Actual score is:        2
+╔═════╦═════╦═════╦═════╗
+║  0  ║  0  ║  0  ║  0  ║
+╠═════╬═════╬═════╬═════╣
+║  0  ║  0  ║  0  ║  0  ║
+╠═════╬═════╬═════╬═════╣
+║  0  ║  0  ║  0  ║  0  ║
+╠═════╬═════╬═════╬═════╣
+║  0  ║  0  ║  0  ║  2  ║
+╚═════╩═════╩═════╩═════╝
 
+*/
 
 namespace cmd {
     void jeu (void) {
         affichage::resizeScreen(SCREEN_WIDTH,SCREEN_HEIGHT);
         affichage::setTitle("Game");
+        Grille grille(grilleVide());
+
+        while(1) {
+            grille = addNumber(grille);
+            grille = deplacement(grille,GAUCHE);
+            setPos(0,3);
+
+            cout << "Actual score is:\t" << score(grille) << endl;
+
+            afficheGrille(grille);
+            exit(0);
+        }
     }
 }
 namespace opengl {
