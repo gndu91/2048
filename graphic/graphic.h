0602fb5eadc4f7c04e7a166527491d4597ae9d23
Ghoul Nadir <nadir.ghoul@u-psud.fr>
2015-11-24 21:53:15 +0100

    Avenced cmd functions

--- graphic/graphic.h
+++ graphic/graphic.h
@@ -7,12 +7,14 @@
         int Main();
         void afficheGrille ( Grille g);
         namespace affichage {
+            void cursor(bool hide);
             void resizeScreen(unsigned short int w,
                               unsigned short int h);
             void setTitle(string Title);
             void drawContent(Grille L1cache,
                              Grille L2cache);
         }
+        short getMove(bool block = false);
     }
 
     namespace sdl {
